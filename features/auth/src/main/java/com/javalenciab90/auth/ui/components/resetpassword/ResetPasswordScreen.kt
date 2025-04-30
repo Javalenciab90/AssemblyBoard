317f42704cd49653d5420e4416c4d688601d2142
Jefferson Roman C. <97921301+jeff1927@users.noreply.github.com>
2025-04-30 13:44:29 -0500

    The reset password screen has been implemented. (#2)

---
+++ features/auth/src/main/java/com/javalenciab90/auth/ui/components/resetpassword/ResetPasswordScreen.kt
@@ -0,0 +1,64 @@
+package com.javalenciab90.auth.ui.components.resetpassword
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.javalenciab90.design_system.theme.AssemblyBoardAppTheme
+import com.javalenciab90.auth.R
+import com.javalenciab90.design_system.theme.Dimens
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ResetPasswordScreen (
+    modifier: Modifier,
+    onHandleIntent: Unit,
+    pageContent: @Composable (PaddingValues) -> Unit
+){
+
+    val context = LocalContext.current
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                modifier = Modifier.wrapContentHeight(),
+                title = { Text("")},
+                navigationIcon = {
+                    Icon(
+                        modifier = Modifier.padding(start = Dimens.All_16).size(Dimens.All_25),
+                        painter = painterResource(id = R.drawable.back_arrow),
+                        contentDescription = "back arrow"
+                    )
+                }
+            )
+        },
+        content = {
+            pageContent(it)
+        }
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ResetPasswordScreenPreview() {
+    AssemblyBoardAppTheme {
+        ResetPasswordScreen(modifier = Modifier, onHandleIntent = Unit) { paddingValues ->
+            ResetPasswordContent (modifier = Modifier.padding(paddingValues)){  }
+        }
+    }
+}
